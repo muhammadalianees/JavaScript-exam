8cec02f437beedb163eb37315157f699c44ce1cd
muhammadalianees <muhammadalianees52@gmail.com>
2023-12-06 20:39:59 +0500

    js test

---
+++ app.js
@@ -0,0 +1,121 @@
+// fruits = ["apple","banana","mango","orange","strawberry"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+
+
+// }
+// for (let j = 0; j < fruits.length; j++) {
+//     console.log(" element is index " + [j] + " is " + fruits[j]);
+
+// }
+
+
+
+// const div = document.querySelector("#text")
+// let bakery = ["cake", "apple pie", "cookie", "chips", "patties"]
+// const item = prompt("Wellcome to ABC bakery. what do you want to oder sir/ma'am?")
+
+// for (let i = 0; i < bakery.length; i++) {
+
+//     if (item == bakery[i]) {
+//         div.innerHTML = item + ' is Available at index ' + i + ' in our bakery '
+
+//     } else {
+//         div.innerHTML = ' We are sorry ' + item + ' is not available in our bakery'
+//     }
+// }
+
+
+
+
+// const div = document.querySelector("#text")
+// var scores = [12, 45, 3, 22, 34, 50]
+
+// const value = +prompt("Enter value")
+// for (let i = 0; i < value; i++)
+// console.log(scores[i])
+
+
+
+
+
+
+
+
+// const div = document.querySelector("#text")
+
+ 
+// let n = 5; 
+// let m = 5;
+
+// let string = "";
+
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < m; j++) {
+//     if (i === 5) {
+//       string += "*";
+//     } else {
+//       if (j === 5) {
+//         string += "*";
+//       } else {
+//         string += "";
+//       }
+//     }
+//   }
+//     string += "\n";
+// }
+// console.log(string);
+
+
+// let firstNumber = +prompt("Enter First Number");
+// let operator = prompt("Operator");
+// let secondNumber = +prompt("Enter Second Number");
+
+// if (operator === "-") {
+//   console.log(firstNumber - secondNumber);
+// } else if (operator === "+") {
+//   console.log(firstNumber + secondNumber);
+// } else if (operator === "*") {
+//   console.log(firstNumber * secondNumber);
+// } else if (operator === "/") {
+//     console.log(firstNumber / secondNumber);
+// }
+
+
+// let userInput = prompt('enter any string');
+
+// function palindromeValue (userInput){
+//     console.log(userInput.split('').reverse().join(''));
+//     if(userInput.split('').reverse().join('') === userInput){
+//         console.log('this is palindrome');
+//     }else{
+//         console.log('false');
+//     }
+
+// }
+// palindromeValue(userInput)
+
+     //Question 07
+
+
+    //  function celsiusToFahrenheit() {
+    //     let celsius = +prompt('Enter Celsius Temperature')
+    //     let b = (celsius * 9 / 5) + 32
+    //     console.log(${celsius}°C is ${b}°F);
+    // }
+    
+    // celsiusToFahrenheit()
+    
+    // function FahrenheitToCelsius() {
+    //     let Fahrenheit = +prompt('Enter Fahrenheit Temperature')
+    //     let b = (Fahrenheit * 9 / 5) + 32
+    //     console.log(${Fahrenheit}°F is ${b}°C);
+    // }
+    
+    // FahrenheitToCelsius()
+    // const  books = [
+    //     {title:"Book1",author:"Author1",price:20},
+    //     {title:"Book2",author:"Author2",price:30},
+    //     {title:"Book3",author:"Author3",price:25},
+    //     {title:"Book4",author:"Author4",price:15},
+    // ]
